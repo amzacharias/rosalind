6f0c10618b299e01c71bb98ec17d69d73eaef8c4
Amanda <77695822+amzacharias@users.noreply.github.com>
2021-04-29 20:09:45 -0500

    rna
    Bioinformatics Stronghold: Transcribing DNA into RNA

---
+++ TranscribingDNAintoRNA.py
@@ -0,0 +1,23 @@
+# This program is for rosalind.info/problems/rna/
+
+# This program will transcribe a DNA string to an RNA string.
+
+# Amanda Zacharias; April 29th, 2021
+
+"""
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.
+"""
+
+DNA_file = open("rosalind_rna.txt","r")
+DNA_s = DNA_file.read()
+
+RNA_s = ""
+for nt in DNA_s:
+    if nt == "T":
+        RNA_s += "U"
+    else:
+        RNA_s += nt
+
+print(RNA_s)
